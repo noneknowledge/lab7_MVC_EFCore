c87ec96fc576c2766e6ba6ce64215bee0ca73e4a
noneknowledge <hoangphuc160802@gmail.com>
2023-08-05 10:44:26 +0700

    cập nhật chức năng create

--- lab7_MVC_EFCore/Controllers/ProductController.cs
+++ lab7_MVC_EFCore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using lab7_MVC_EFCore.Data;
+using lab7_MVC_EFCore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -24,9 +25,24 @@ namespace lab7_MVC_EFCore.Controllers
         {
             ViewBag.Suppliers = new SelectList( _context.Suppliers.ToList(),"Id","Name");
             ViewBag.Categories = new SelectList(_context.Categories.ToList(),"Id","Name") ;
-
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Product model, IFormFile ProductImage)
+        {
+            if (ProductImage != null)
+            {
+                //upload và cập nhật field Logo
+                model.Image = MyTool.UploadImageToFolder(ProductImage, "Products");
+            }
+            _context.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
 
     }
 }
